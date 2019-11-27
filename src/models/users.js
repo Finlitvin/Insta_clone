8955622ea40d10046216e8638377b900caa6d7c5
Finlitvin <ros.litvin@gmail.com>
2019-11-27 19:41:59 +0300

    [models]: add user

---
+++ src/models/users.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../database/sequelize');
+
+const User = sequelize.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    first_name: {
+        type: Sequelize.STRING
+    },
+    last_name: {
+        type: Sequelize.STRING
+    },
+})
+export default User;
