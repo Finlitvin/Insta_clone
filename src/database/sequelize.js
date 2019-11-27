f9e08e4d97c5b9aae0806715db30af11a9a3f554
Finlitvin <ros.litvin@gmail.com>
2019-11-27 22:53:48 +0300

    add authenticate() | tests the connection by trying to auth to the database

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -3,4 +3,12 @@ const config = require('../config/sequelize');
 
 const sequelize = new Sequelize('instagram', 'root', 'root', config);
 
+sequelize.authenticate().then(() => {
+    console.log('Connection established successfully.');
+}).catch(err => {
+    console.error('Unable to connect to the database:', err);
+}).finally(() => {
+    sequelize.close();
+});
+
 export default sequelize;
