c2f3955660dc79801ab349cdb6103efaa2b38bf9
Finlitvin <ros.litvin@gmail.com>
2019-12-16 18:43:44 +0300

    fix sequelize.js

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -3,12 +3,13 @@ const config = require('../config/sequelize');
 
 const sequelize = new Sequelize('instagram', 'root', 'root', config);
 
+// test connection to DB
 sequelize.authenticate().then(() => {
-    console.log('Connection established successfully.');
+    console.log('Connection established successfully: ', config.host);
 }).catch(err => {
     console.error('Unable to connect to the database:', err);
 }).finally(() => {
     sequelize.close();
 });
 
-export default sequelize;
+module.exports = sequelize;
