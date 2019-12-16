91960181507d53f4fcd473118814d54f5e4fc822
Finlitvin <ros.litvin@gmail.com>
2019-12-16 18:36:29 +0300

    [models]: FIX relationship

--- src/models/relationship.js
+++ src/models/relationship.js
@@ -1,32 +1,26 @@
-const Users = require('./users');
-const Posts = require('./posts');
-const UsersRoles = require('./users_roles');
-const Roles = require('./roles');
-const Likes = require('./likes');
-const Tags = require('./tags');
-const PostsTags = require('./posts_tags');
-
+DB = require('../init/initDataBase.js');
+const Sequelize = require('sequelize');
 // ------Hint-----
 // belongsTo - one-to-one
 // hasMany - many-to-one
-// belongsToMany - one-to-many
+// belongsToMany - many-to-many
 
-// users - users_roles - roles
-Users.belongsToMany(Roles, {through: UsersRoles, foreignKey: 'user_id', onDelete: 'SET NULL'});
-Roles.belongsToMany(Users, {through: UsersRoles, foreignKey: 'role_id', onDelete: 'SET NULL'});
+    // users - users_roles - roles
+    DB.Users.belongsToMany(DB.Roles, {through: DB.UsersRoles, foreignKey: 'user_id', onDelete: 'SET NULL'});
+    DB.Roles.belongsToMany(DB.Users, {through: DB.UsersRoles, foreignKey: 'role_id', onDelete: 'SET NULL'});
 
-// posts - posts_tags - tags
-Posts.belongsToMany(Tags, {through: PostsTags, foreignKey: 'post_id', onDelete: 'SET NULL'});
-Tags.belongsToMany(Posts, {through: PostsTags, foreignKey: 'tag_id', onDelete: 'SET NULL'});
+    // posts - posts_tags - tags
+    DB.Posts.belongsToMany(DB.Tags, {through: DB.PostsTags, foreignKey: 'post_id', onDelete: 'SET NULL'});
+    DB.Tags.belongsToMany(DB.Posts, {through: DB.PostsTags, foreignKey: 'tag_id', onDelete: 'SET NULL'});
 
-// users - likes
-Users.hasMany(Likes, {foreignKey: 'user_id', onDelete: 'CASCADE'});
-Likes.belongsTo(Users, {foreignKey: 'user_id', onDelete: 'CASCADE'});
+    // users - likes
+    DB.Users.hasMany(DB.Likes, {foreignKey: 'user_id', onDelete: 'CASCADE'});
+    DB.Likes.belongsTo(DB.Users, {foreignKey: 'user_id', onDelete: 'CASCADE'});
 
-// users - posts
-Users.hasMany(Posts, {foreignKey: 'user_id', onDelete: 'CASCADE'});
-Posts.belongsTo(Users, {foreignKey: 'user_id', onDelete: 'CASCADE'});
+    // users - posts
+    DB.Users.hasMany(DB.Posts, {foreignKey: 'user_id', onDelete: 'CASCADE'});
+    DB.Posts.belongsTo(DB.Users, {foreignKey: 'user_id', onDelete: 'CASCADE'});
 
-// posts - likes
-Posts.hasMany(Likes, {foreignKey: 'post_id', onDelete: 'CASCADE'});
-Likes.belongsTo(Posts, {foreignKey: 'post_id', onDelete: 'CASCADE'});
+    // posts - likes
+    DB.Posts.hasMany(DB.Likes, {foreignKey: 'post_id', onDelete: 'CASCADE'});
+    DB.Likes.belongsTo(DB.Posts, {foreignKey: 'post_id', onDelete: 'CASCADE'});
