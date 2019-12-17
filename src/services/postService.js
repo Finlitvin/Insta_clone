6a9824ba2a88c237adbb6b3a6d3df4124ad77212
Finlitvin <ros.litvin@gmail.com>
2019-12-17 17:27:10 +0300

    [API CREATE Post]: add post service

---
+++ src/services/postService.js
@@ -0,0 +1,10 @@
+const postRepository = require('../repositories/postRepository');
+
+class PostService{
+    async addPost(post){
+
+        await postRepository.addPost(post);
+    }
+}
+
+module.exports = new PostService();
