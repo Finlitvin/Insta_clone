36fc65bbf7663a94190f602ad117d8c991ab71bf
Finlitvin <ros.litvin@gmail.com>
2019-12-20 13:40:08 +0300

    [API GET_ALL Post]: add routes

--- src/routes/postRout.js
+++ src/routes/postRout.js
@@ -2,7 +2,7 @@ const express = require('express');
 const postController = require('../controllers/postController');
 const router = express.Router();
 
-
+router.get('/all', postController.getAllPost);
 router.post('/', postController.addPost);
 router.delete('/:id', postController.deletePost);
 
