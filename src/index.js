36ce7d6aa107cdaf49358f06b1c7feef799fd9d3
Finlitvin <ros.litvin@gmail.com>
2019-12-29 17:41:11 +0300

    [API Tag]: add route to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express();
 
 const postRouter = require('./routes/postRout');
 const roleRouter = require('./routes/roleRout');
+const tagRouter = require('./routes/tagRout');
 
 const bp = require('body-parser');
 
@@ -13,7 +14,7 @@ app.use(bp());
 
 app.use('/post', postRouter);
 app.use('/role', roleRouter);
-
+app.use('/tag', tagRouter);
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`SERVER [ OK ]    PORT ${process.env.PORT || PORT}`);
