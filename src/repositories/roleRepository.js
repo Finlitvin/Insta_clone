f70cd213e4f0aeb5ef1e9bab6ef4ffce3c254c37
Finlitvin <ros.litvin@gmail.com>
2019-12-29 15:28:01 +0300

    [API Role]: add repository

---
+++ src/repositories/roleRepository.js
@@ -0,0 +1,34 @@
+const Role = require('../models/roles');
+
+class RoleRepository{
+    async getRole(id) {
+        const role = await Role.findByPk(id);
+    
+        return role;
+      }
+    
+      async getRoleByValue(value) {
+        const role = await Role.findOne({ where: { value: value } });
+    
+        return role;
+      }
+    
+      async getAllRoles() {
+        const roles = await Role.findAll();
+    
+        return roles;
+      }
+    
+      async addRole(role) {
+        await Role.create(role);
+      }
+    
+      async deleteRole(id) {
+        await Role.destroy({ where: { id: id } });
+      }
+    
+      async updateRole(id, newRoleData) {
+        const role = await this.getRole(id);
+        await role.update(newRoleData);
+      }
+}
