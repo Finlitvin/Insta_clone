f29da3189f1c422ac3bf1b0e8cf7a3aa4271ff0b
Finlitvin <ros.litvin@gmail.com>
2019-12-29 17:23:55 +0300

    [API Tag]: add repository

---
+++ src/repositories/tagRepository.js
@@ -0,0 +1,38 @@
+const Tag = require('../models/tags');
+
+class TagRepository{
+
+    async getTag(id) {
+        const tag = await Tag.findByPk(id);
+    
+        return tag;
+      }
+    
+      async getTagByValue(value) {
+        const tag = await Tag.findOne({ where: { value: value } });
+    
+        return tag;
+      }
+    
+      async getAllTags() {
+        const tags = await Tag.findAll();
+    
+        return tags;
+      }
+    
+      async addTag(role) {
+        await Tag.create(role);
+      }
+    
+      async deleteTag(id) {
+        await Tag.destroy({ where: { id: id } });
+      }
+    
+      async updateTag(id, newTagData) {
+        const tag = await this.getRole(id);
+        await tag.update(newTagData);
+      }
+
+}
+
+module.exports = new TagRepository();
