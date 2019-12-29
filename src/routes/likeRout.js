f9e3c9e40cc2cfb656a27ded454cd77fa67c51c8
Finlitvin <ros.litvin@gmail.com>
2019-12-29 21:18:38 +0300

    [API Like]: add route

---
+++ src/routes/likeRout.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const likeController = require('../controllers/likeController');
+
+const router = express.Router();
+
+
+router.post('/', likeController.addLike);
+router.get('/my', likeController.getMyLike);
+router.delete('/:id', likeController.deleteLike);
+
+
+router.get('/', likeController.getAllLikes);
+router.get('/:id', likeController.getLike);
+
+router.get('/films/:id', likeController.getLike);
+
+
+module.exports = router;
